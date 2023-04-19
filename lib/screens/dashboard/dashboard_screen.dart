edeb52d32548bf0457a7073cc1fe63efd54ecf4e
HP <rupeshgour31@gmail.com>
2023-04-19 18:48:17 +0530

    add fingerprint login and dashboard bottom bar

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yolo/utils/app_colors.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class DashboardScreen extends StatefulWidget {
   static const String route = "DashboardScreen";
@@ -11,14 +12,98 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Likes',
+      style: optionStyle,
+    ),
+    Text(
+      'Search',
+      style: optionStyle,
+    ),
+    Text(
+      'Profile',
+      style: optionStyle,
+    ),Text(
+      'Profile',
+      style: optionStyle,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.whiteBgColor,
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: AppColors.whiteBgColor,
-        elevation: 0,
-        automaticallyImplyLeading: false,
+        elevation: 20,
+        title: const Text('GoogleNavBar'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: Container(
+        margin: const EdgeInsets.fromLTRB(15,0,15,20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 10,
+              color: AppColors.blackFont.withOpacity(.2),
+            )
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 8),
+            child: GNav(
+              rippleColor: Colors.white,
+              hoverColor: Colors.white,
+              gap: 8,
+              activeColor: AppColors.whiteColor,
+              iconSize: 22,
+              padding:const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              duration:const Duration(milliseconds: 400),
+              tabBackgroundColor: AppColors.blackFont,
+              color: AppColors.blackFont,
+              tabs: const [
+                GButton(
+                  icon: Icons.home_filled,
+                  text: 'Home',
+                ),
+                GButton(
+                  icon: Icons.wallet,
+                  text: 'Likes',
+                ),
+                GButton(
+                  icon: Icons.share,
+                  text: 'Search',
+                ),
+                GButton(
+                  icon: Icons.people_alt_outlined,
+                  text: 'Search',
+                ),
+                GButton(
+                  icon: Icons.calendar_today_outlined,
+                  text: 'Profile',
+                ),
+              ],
+              selectedIndex: _selectedIndex,
+              onTabChange: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            ),
+          ),
+        ),
       ),
     );
   }
